bd051f93a0443cd7bcc9cd8a87afda8615db4515
eualex <allex.araujo088@gmail.com>
2021-09-13 16:17:27 -0300

    feat: add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express, { json } from 'express'
+import swaggerUi from 'swagger-ui-express'
 
 import { routes } from './routes'
+import swaggerFile from './swagger.json'
 
 const app = express()
 
 app.use(json())
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(routes)
 
 app.listen(3333, () => console.log('Server is running!'))
